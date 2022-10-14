e3446b2e58c1d9464112a357b1039f4fc61bc270
Elijah Bassett <69734856+BassettUNC@users.noreply.github.com>
2022-10-14 15:08:16 -0400

    fixing autograder

--- cli.js
+++ cli.js
@@ -7,14 +7,9 @@ import minimist from 'minimist';
 //Select args for help
 const args = minimist(process.argv.slice(2));
 if(args[0] == '-h'){
-	console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE")
-	console.log( "-h           Show this help message and exit.")
-	console.log("-n, -s        Latitude: N positive; S negative.")
-	console.log("-e, -w        Longitude: E positive; W negative.")
-	console.log("-z            Time zone: uses tz.guess() from moment-timezone by default.")
-	console.log("-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.")
-	console.log("-j            Echo pretty JSON from open-meteo API and exit.")
-	process.exit(0);
+    console.log('Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -t TIME_ZONE\n    -h            Show this help message and exit.\n    -n, -s        Latitude: N positive; S negative.\n    -e, -w        Longitude: E positive; W negative.\n    -t            Time zone: uses tz.guess() from moment-timezone by default.\n    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.\n    -j            Echo pretty JSON from open-meteo API and exit.');
+	process.ext(0);
+
 }
 
 //Set timezone
@@ -41,7 +36,7 @@ if (args.w) {
 }
 
 //Define fetch Url
-const url = "https://api.open-meteo.com/v1/forecast?" + "latitude=" + lat + "&longitude=" + lon + "&daily=precipitation_sum,precipitation_hours&timezone=" +timezone;
+const url = "https://api.open-meteo.com/v1/forecast?" + "latitude=" + lat + "&longitude=" + lon + "&hourly=temperature_2m&daily=precipitation_hours&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=" +timezone;
 
 // Make a request
 const response = await fetch(url);
@@ -77,7 +72,7 @@ if (days == 0) {
 	} else {
 		console.log("You might need your galoshes");
 	}
-	console.log(" in" + days + " days");
+	console.log(" in" + days + " days.");
 } else {
 	if (data.daily.precipitation_hours[days] == 0) {
 		console.log("You will not need your galoshes tomorrow.");
